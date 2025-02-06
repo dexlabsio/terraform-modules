1a37de4d26d0ad8b8db9a429e70637ae786f36fa
Matheus Miranda <matheusmirandalacerda@gmail.com>
2025-02-06 15:36:33 -0300

    [CLOUD-SHELL] Update serviceaccount setup script

--- gcp/cloud-shell/bigquery-service-account-setup.py
+++ gcp/cloud-shell/bigquery-service-account-setup.py
@@ -74,19 +74,23 @@ if f"projects/{PROJECT_ID}/roles/{CUSTOM_ROLE_ID}" in existing_roles:
 else:
     print("Creating custom storage role...")
     storage_permissions = [
-        "storage.managedFolders.delete",
-        "storage.managedFolders.get",
-        "storage.managedFolders.list",
-        "storage.multipartUploads.abort",
-        "storage.multipartUploads.create",
-        "storage.multipartUploads.list",
-        "storage.multipartUploads.listParts",
-        "storage.objects.create",
-        "storage.objects.delete",
-        "storage.objects.get",
-        "storage.objects.list",
-        "storage.objects.restore",
-        "storage.objects.update"
+        "bigquery.datasets.create",
+        "bigquery.datasets.get",
+        "bigquery.datasets.getIamPolicy",
+        "bigquery.jobs.create",
+        "bigquery.models.getMetadata",
+        "bigquery.models.list",
+        "bigquery.routines.get",
+        "bigquery.routines.list",
+        "bigquery.tables.create",
+        "bigquery.tables.get",
+        "bigquery.tables.getData",
+        "bigquery.tables.getIamPolicy",
+        "bigquery.tables.list",
+        "bigquery.tables.update",
+        "bigquery.tables.updateData",
+        "dataplex.projects.search",
+        "resourcemanager.projects.get"
     ]
 
     role_definition = {
@@ -131,7 +135,6 @@ else:
 
 # Step 5: Generate service account key JSON file
 print("Checking if service account key exists...")
-
 existing_keys = run_command([
     "gcloud", "iam", "service-accounts", "keys", "list",
     "--iam-account", SERVICE_ACCOUNT_EMAIL,
@@ -141,28 +144,29 @@ existing_keys = run_command([
 ])
 
 if existing_keys:
-    print("Service account key already exists, skipping creation.")
-    run_command([
-        "gcloud", "iam", "service-accounts", "keys", "list",
-        "--iam-account", SERVICE_ACCOUNT_EMAIL,
-        "--project", PROJECT_ID,
-        "--managed-by", "user",
-    ])
-else:
-    print("Generating service account key...")
-    run_command([
-        "gcloud", "iam", "service-accounts", "keys", "create", KEY_FILE_NAME,
-        "--iam-account", SERVICE_ACCOUNT_EMAIL,
-        "--project", PROJECT_ID
-    ], check=True)
+    print("Deleting existing service account keys...")
+    for key in existing_keys.split("\n"):
+        run_command([
+            "gcloud", "iam", "service-accounts", "keys", "delete", key,
+            "--iam-account", SERVICE_ACCOUNT_EMAIL,
+            "--project", PROJECT_ID,
+            "--quiet"
+        ], check=True)
 
-    # Read and print the key content
-    with open(KEY_FILE_NAME, 'r') as key_file:
-        key_content = json.load(key_file)
-        print("\nService Account Key JSON:\n")
-        print(json.dumps(key_content, indent=2))
+print("Generating new service account key...")
+run_command([
+    "gcloud", "iam", "service-accounts", "keys", "create", KEY_FILE_NAME,
+    "--iam-account", SERVICE_ACCOUNT_EMAIL,
+    "--project", PROJECT_ID
+], check=True)
 
-    # Optional: Remove the key file after printing
-    os.remove(KEY_FILE_NAME)
+# Read and print the key content
+with open(KEY_FILE_NAME, 'r') as key_file:
+    key_content = json.load(key_file)
+    print("\nService Account Key JSON:\n")
+    print(json.dumps(key_content, indent=2))
 
-print("Service account and roles assigned successfully!")
+# Optional: Remove the key file after printing
+os.remove(KEY_FILE_NAME)
+
+print("Service account key successfully generated!")
